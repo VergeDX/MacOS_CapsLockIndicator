3a349c74299dc959c724e098d58efaac4d3a57bf
VergeDX <wxy20001225@126.com>
2020-09-03 20:39:41 +0800

    [+] Init commit.

---
+++ build.gradle.kts
@@ -0,0 +1,31 @@
+@file:Suppress("SpellCheckingInspection")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins { kotlin("jvm") version "1.4.0" }
+
+group = "org.hydev"
+version = "1.0-SNAPSHOT"
+
+repositories { mavenCentral() }
+tasks.withType<KotlinCompile> { kotlinOptions.jvmTarget = "1.8" }
+
+dependencies {
+    // https://github.com/kristian/system-hook
+    implementation("com.1stleg:jnativehook:2.1.0")
+
+    // https://github.com/zeroturnaround/zt-exec
+    implementation("org.zeroturnaround:zt-exec:1.12")
+}
+
+// https://dev.to//preslavrachev/create-executable-kotlin-jars-using-gradle
+// https://www.cnblogs.com/mengdd/p/android-gradle-migrate-from-groovy-to-kotlin.html
+// https://gist.github.com/domnikl/c19c7385927a7bef7217aa036a71d807
+apply(plugin = "application")
+val jar by tasks.getting(Jar::class) {
+    // Modify main class here!
+    manifest { attributes["Main-Class"] = "org.hydev.MainKt" }
+
+    // https://stackoverflow.com/questions/46157338/using-gradle-to-build-a-jar-with-dependencies-with-kotlin-dsl
+    configurations["compileClasspath"].forEach { from(zipTree(it.absoluteFile)) }
+}
---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,59 @@
+@file:JvmName("MainKt")
+
+package org.hydev
+
+import org.jnativehook.GlobalScreen
+import org.jnativehook.keyboard.NativeKeyEvent
+import org.jnativehook.keyboard.NativeKeyListener
+import org.zeroturnaround.exec.ProcessExecutor
+import java.awt.*
+import java.awt.Toolkit.getDefaultToolkit
+import java.awt.event.KeyEvent
+import java.util.logging.Level
+import java.util.logging.Logger
+import kotlin.system.exitProcess
+
+lateinit var currentIcon: TrayIcon
+
+class GlobalKeyListener : NativeKeyListener {
+    override fun nativeKeyTyped(p0: NativeKeyEvent?) {}
+    override fun nativeKeyPressed(p0: NativeKeyEvent?) {}
+
+    override fun nativeKeyReleased(p0: NativeKeyEvent?) {
+        if (p0?.keyCode == NativeKeyEvent.VC_CAPS_LOCK)
+            currentIcon.image = getTrayImage()
+    }
+}
+
+fun main() {
+    // Hide java icon in dock.
+    System.setProperty("apple.awt.UIElement", "true")
+
+    // Disable debug log.
+    val logger = Logger.getLogger(GlobalScreen::class.java.getPackage().name)
+    logger.level = Level.WARNING
+
+    // Build quit menu.
+    val quitMenuItem = MenuItem("Quit").apply { addActionListener { exitProcess(0) } }
+    val popupMenu = PopupMenu().apply { add(quitMenuItem) }
+
+    // Set tray icon & popup menu.
+    currentIcon = TrayIcon(getTrayImage(), "Caps lock Indicator")
+    currentIcon.popupMenu = popupMenu
+    SystemTray.getSystemTray().add(currentIcon)
+
+    // Register keyboard hook and add listener.
+    GlobalScreen.registerNativeHook()
+    GlobalScreen.addNativeKeyListener(GlobalKeyListener())
+}
+
+fun getTrayImage(): Image {
+    val capsLockStatusString = if (getDefaultToolkit().getLockingKeyState(KeyEvent.VK_CAPS_LOCK)) "On" else "Off"
+    val styleModeString = if (ProcessExecutor().command("defaults", "read", "-g", "AppleInterfaceStyle")
+            .readOutput(true).execute().outputUTF8() == "Dark\n"
+    ) "Dark" else "Light"
+
+    // On_Dark.png | On_Light.png | Off_Dark.png | Off_Light.png
+    val fileName = "${capsLockStatusString}_$styleModeString.png"
+    return getDefaultToolkit().createImage(object {}.javaClass.getResource("/$fileName").readBytes())
+}
